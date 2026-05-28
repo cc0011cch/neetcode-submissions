3f658a0b3372b1b650b3670fb25f554e8646ff8b
cc0011cch <chan.chiho7@googlemail.com>
2026-05-28 17:05:05 +0100

    Add: find-target-in-rotated-sorted-array - submission-17

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-17.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target: 
+                return mid
+            
+            # 步驟 1：如果 nums[mid] >= nums[l]，代表「左半邊」是正常遞增排序的
+            if nums[l] <= nums[mid]:
+                # 步驟 2：檢查 target 是否在左半邊正常區間內
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1  # 步驟 3：在左邊，把右指針拉過來
+                else:
+                    l = mid + 1  # 不在左邊，去右邊找
+                    
+            # 步驟 1：否則，代表「右半邊」是正常遞增排序的
+            else:
+                # 步驟 2：檢查 target 是否在右半邊正常區間內
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1  # 步驟 3：在右邊，把左指針拉過去
+                else:
+                    r = mid - 1  # 不在右邊，去左邊找
+
+        return -1
