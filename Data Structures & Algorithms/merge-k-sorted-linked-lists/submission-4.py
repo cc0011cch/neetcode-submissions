13bdafbd1752bf86040a10e8bc12b1c7e84618bc
cc0011cch <chan.chiho7@googlemail.com>
2026-07-10 15:28:45 +0100

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+        
+    def mergeTwoLists(self,l1: Optional[ListNode], l2: Optional[ListNode])-> Optional[ListNode]:
+ #           if l1 is None and l2 is None : return l1
+ #           if l1 is None : return l2
+ #           if l2 is None : return l1
+
+ #           if l1.val< l2.val:
+ #               l1.next = mergeTwoLists(l1.next,l2)
+ #               return l1
+ #           else:
+ #               l2.next = mergeTwoLists(l1,l2.next)
+ #               return l2
+            dummy = ListNode(0)            
+            thead =dummy;
+            while l1 and l2:
+                if l1.val< l2.val:
+                    thead.next =l1
+                    l1=l1.next
+                else:
+                    thead.next =l2
+                    l2=l2.next
+                thead=thead.next
+
+            if l1: thead.next=l1
+            if l2: thead.next=l2
+            return dummy.next
+
+    def divideAndConquer(self, lists: List[Optional[ListNode]], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return lists[left]
+        
+        mid = (left + right) // 2
+        l1 = self.divideAndConquer(lists, left, mid)
+        l2 = self.divideAndConquer(lists, mid + 1, right)
+        
+        return self.mergeTwoLists(l1, l2)
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+
+#        chead = lists[0]
+#        for i in range(1,len(lists)):
+#            chead=self.mergeTwoLists(chead,lists[i])
+#        return chead
+        return self.divideAndConquer(lists,0,len(lists)-1)        
